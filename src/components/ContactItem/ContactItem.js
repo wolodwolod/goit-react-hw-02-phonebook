147bb0f107201bf655fdc7d13e94e9e842b8a238
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-02-16 00:21:08 +0200

    Prop

--- src/components/ContactItem/ContactItem.js
+++ src/components/ContactItem/ContactItem.js
@@ -19,6 +19,7 @@ const ContactItem = ({ id, name, number, onDelete}) => {
 ContactItem.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  
+  number: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 export default ContactItem;
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -23,9 +23,11 @@ ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
           })
-  )
-  
-};
+    ),
+    onDelete: PropTypes.func.isRequired,
+  };
+
 export default ContactList;
